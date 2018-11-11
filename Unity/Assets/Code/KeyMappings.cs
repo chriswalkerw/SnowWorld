1887aa033a321667725abbaefd13f9fb2e031e47
Christopher Walker <chris.jaguar@gmail.com>
2018-11-11 15:35:04 -0800

    Add the main camera controller

---
+++ Unity/Assets/Code/KeyMappings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class KeyMappings {
+    #region Camera
+    public static int InitiateRotatingMouseCode = 1;        // Right Mouse Button
+    public static KeyCode PanForwardKeyCode = KeyCode.W;
+    public static KeyCode PanBackwardsKeyCode = KeyCode.S;
+    public static KeyCode PanLeftKeyCode = KeyCode.A;
+    public static KeyCode PanRightKeyCode = KeyCode.D;
+    #endregion
+}
---
+++ Unity/Assets/Code/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Settings
+{
+    #region Camera
+    public static float MoveSpeed = 15.0f;
+    public static float MinFov = 1f;
+    public static float MaxFov = 120f;
+    public static float ZoomSensitivity = 20f;
+    public static Vector2 ClampInDegrees = new Vector2(360, 180);
+    public static Vector2 RotateSensitivity = new Vector2(2, 2);
+    public static Vector2 RotateSmoothing = new Vector2(3, 3);
+    #endregion
+}
