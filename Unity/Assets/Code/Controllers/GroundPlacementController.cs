aab70584687f180641df98565a0dc3b5d550b779
Christopher Walker <chris.jaguar@gmail.com>
2018-11-11 15:42:56 -0800

    Projet structure change

---
+++ Unity/Assets/Code/Controllers/GroundPlacementController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+// https://unity3d.college/2017/08/02/how-to-create-a-unity3d-building-placement-system-for-rts-or-city-builders-let-your-player-place-a-3d-object-in-the-world/
+public class GroundPlacementController : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject placeableObjectPrefab;
+
+    [SerializeField]
+    public KeyCode newObjectHotkey = KeyCode.N;
+
+    private GameObject currentPlaceableObject;
+
+    private float mouseWheelRotation;
+
+    private void Update()
+    {
+        HandleNewObjectHotkey();
+
+        if (currentPlaceableObject != null)
+        {
+            MoveCurrentObjectToMouse();
+            RotateFromMouseWheel();
+            ReleaseIfClicked();
+        }
+    }
+
+    private void HandleNewObjectHotkey()
+    {
+        if (Input.GetKeyDown(newObjectHotkey))
+        {
+            if (currentPlaceableObject != null)
+            {
+                Destroy(currentPlaceableObject);
+            }
+            else
+            {
+                currentPlaceableObject = Instantiate(placeableObjectPrefab);
+            }
+        }
+    }
+
+    private void MoveCurrentObjectToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            currentPlaceableObject.transform.position = hitInfo.point;
+            currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+    }
+
+    private void RotateFromMouseWheel()
+    {
+        mouseWheelRotation += Input.mouseScrollDelta.y;
+        currentPlaceableObject.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
+    }
+
+    private void ReleaseIfClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentPlaceableObject = null;
+        }
+    }
+}
